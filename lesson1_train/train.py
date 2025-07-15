c760785b2476b607075393f205f2d4c24ed46614
Zhenya-Sim <simonova.evd21597@gmail.com>
2025-07-15 17:38:20 +0400

    first commit

---
+++ lesson1_train/train.py
@@ -0,0 +1,29 @@
+my_heigh = 165
+print(my_heigh)
+
+my_name = "Женя"
+my_name = "Женя Симонова"
+print(my_name)
+
+pet_name = input("Как зовут Вашего питомца?")
+print("Ваш любимчик - " + pet_name)
+
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+
+def print_letter(let):
+    print(let, end="")
+
+
+print_letter("S")
+print_letter("t")
+print_letter("u")
+print_letter("d")
+print_letter("e")
+print_letter("n")
+print_letter("t")
